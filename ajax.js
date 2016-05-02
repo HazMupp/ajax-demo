cf0605f0460ce97601a53ba80c921cf7e2fc5690
HazMupp <peter_heier@hotmail.com>
2016-05-02 11:38:19 +0200

    Created ajax.js

---
+++ ajax.js
@@ -0,0 +1,19 @@
+// JavaScript med AJAX
+// Vi använder AJAX för att hämta innehållet på en hemsida
+
+// Vad är AJAX?
+// Det är inte ett programmeringsspråk...
+
+// ...det är en teknik, en kombination av saker, för att hämta data från URLs
+// AJAX - Asyncronous JavaScript and XML
+
+// Påbörja en hämtning
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+console.log("någonting hände");
+
+}
+
+request.open('GET', 'http://www.google.com');
+request.send();
